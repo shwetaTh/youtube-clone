c9f0c122074f80d45f279e187a9dc6fa41972673
shwetaTh <shwetaTh@users.noreply.github.com>
2024-06-01 13:56:25 +0545

    utils, error and response handling standard

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
     path: './env'
 })
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`server is running at port: ${process.env.PORT}`);
+    })
+})
+.catch((error)=>{
+    console.log(`MongoDb connection failed. ${error}`);
+})
 
 
 /* use this if the db index doenst work
