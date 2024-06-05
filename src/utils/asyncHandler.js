dc57179fb28defa08f84e2949eebe2c38726cd77
shwetaTh <shwetaTh@users.noreply.github.com>
2024-06-05 19:30:39 +0545

    set up user router and fixed minor import error

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,20 +1,20 @@
-// const asyncHandler = (reqHandler)=> {
-//     (req, res, next) =>{
-//         Promise.resolve(reqHandler(req, res, next)).catch((err) => next(err))
-//     }
-// }
-
-
-const asyncHandler = (fn)=> async (req, res, next)=>{
-    try {
-        await fn(req, res, next)
-    } catch (error) {
-        res.status(error.code || 500).json({
-            success: false,
-            message: error.message
-        })
+const asyncHandler = (reqHandler)=> {
+    return (req, res, next) =>{
+        Promise.resolve(reqHandler(req, res, next)).catch((err) => next(err))
     }
 }
+
+
+// const asyncHandler = (fn)=> async (req, res, next)=>{
+//     try {
+//         await fn(req, res, next)
+//     } catch (error) {
+//         res.status(error.code || 500).json({
+//             success: false,
+//             message: error.message
+//         })
+//     }
+// }
 
 //()=>{()=>{}}
 
